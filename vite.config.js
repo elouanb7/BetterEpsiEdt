02187b8e842b3cb4b5f991ef56b6701e302bf319
elouanb7 <elouanb7@gmail.com>
2024-04-25 10:56:29 +0200

    Fixed build.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Specify the base path
+  build: {
+    outDir: 'dist', // Output directory
+    assetsDir: 'assets', // Assets directory (no leading slash)
+    // Optionally configure the public path if needed
+    publicDir: './',
+  },
 })
