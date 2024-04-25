162c4a9e383861e53edcd58c0c053e935a259caf
elouanb7 <elouanb7@gmail.com>
2024-04-25 15:53:40 +0200

    Try to do it without CORS extension

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './', // Specify the base path
-  build: {
-    outDir: 'dist', // Output directory
-    assetsDir: 'assets', // Assets directory (no leading slash)
-    // Optionally configure the public path if needed
-    publicDir: './',
-  },
-  preview: {
-    host: 'localhost',
-    port: 5173,
-    strictPort: true,
-  },
+    plugins: [vue()],
+    base: './', // Specify the base path
+    build: {
+        outDir: 'dist', // Output directory
+        assetsDir: 'assets', // Assets directory (no leading slash)
+        // Optionally configure the public path if needed
+        publicDir: './',
+    },
+    preview: {
+        host: 'localhost',
+        port: 5173,
+        strictPort: true,
+        proxy: 'https://edtmobiliteng.wigorservices.net',
+    },
+    server: {
+        proxy: {
+            '/edt': {
+                target: 'tonurl',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/edt/, '')
+            }
+        }
+    }
 })
